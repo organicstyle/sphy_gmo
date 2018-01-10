cb95c7b95ebe39772485ae7ddfe56296f9fcbdb4
Oki Ohashi <okiohashi@icloud.com>
2018-01-10 18:10:10 +0900

    設定ファイルの修正

--- spec/sphy_gmo/card_spec.rb
+++ spec/sphy_gmo/card_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
-require 'yaml'
-
-settings = YAML.load_file("settings.yml")
 
 SphyGmo.configure do |config|
-  config.host = settings["GMO_HOST"]
-  config.site_id = settings["GMO_SITE_ID"]
-  config.site_pass = settings["GMO_SITE_PASS"]
-  config.shop_id = settings["GMO_SHOP_ID"]
-  config.shop_pass = settings["GMO_SHOP_PASS"]
+  config.host = SPEC_CONF["host"]
+  config.site_id = SPEC_CONF["site_id"]
+  config.site_pass = SPEC_CONF["site_pass"]
+  config.shop_id = SPEC_CONF["shop_id"]
+  config.shop_pass = SPEC_CONF["shop_pass"]
 end
