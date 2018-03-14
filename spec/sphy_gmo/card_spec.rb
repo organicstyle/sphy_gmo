48db2589e41e9b940a142be4371165bae6b0024d
Oki Ohashi <okiohashi@icloud.com>
2018-03-14 15:00:18 +0900

    member_idをletで宣言

--- spec/sphy_gmo/card_spec.rb
+++ spec/sphy_gmo/card_spec.rb
@@ -6,7 +6,6 @@ I18n.default_locale = :ja
 
 describe SphyGmo::Card do
   before do
-    @member_id = generate_id.to_s
     SphyGmo.configure do |config|
       config.host = ENV['MANATSUKU_GMO_HOST']
       config.site_id = ENV['MANATSUKU_GMO_SITE_ID']
@@ -18,22 +17,22 @@ describe SphyGmo::Card do
 
   #GMOペイメント会員登録
   describe "#Member.save" do
+    let(:member_id) { generate_id.to_s }
     it "gets data about a member" do
-      member_id = @member_id
       member_name = "Foo Bar"
       SEQ_MODE = 0
       SphyGmo::Member.save!( member_id: member_id, member_name: member_name, seq_mode: SEQ_MODE )
       member = SphyGmo::Member.search!(member_id: member_id)
-      expect(member).to include("MemberID" => @member_id, "MemberName" => member_name, "DeleteFlag" => "0")
+      expect(member).to include("MemberID" => member_id, "MemberName" => member_name, "DeleteFlag" => "0")
     end
   end
 
   #無効なトークンでは登録できないことを確認
   describe "#Card.save" do
+    let(:member_id) { generate_id.to_s }
     it "gets data about a card" do
       error_code = I18n.t :token_error
       expect do
-        member_id = @member_id
         token = generate_token
         SphyGmo::Card.save!( member_id: member_id, token: token, default_flag: 1, seq_mode: SEQ_MODE )
       end.to raise_error(SphyGmo::APIError, error_code)
